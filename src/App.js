d58141e43a870d76ca2e3654176d4a400a58db1c
Santiago <santib.2001@hotmail.com>
2024-05-06 20:04:15 -0500

    feat: Mocking 1 response to avoid unneccessary calls

--- src/App.js
+++ src/App.js
@@ -8,15 +8,35 @@ function App() {
   const [products, setProducts] = useState([]);
   const { getProducts } = useContentful();
 
-  useEffect(() => {
-    getProducts().then((response) => response && setProducts(response));
-    console.log('Fetching');
-  });
+  /* useEffect(() => {
+    getProducts().then((response) => {
+      response && setProducts(response)
+      console.log(response);
+    });
+  }); */
+
+  const product = [{
+    name: "Caramel Flan",
+    description: "Basically a Flan, but caramelized.",
+    avatar: {
+      title: 'Caramel Flan',
+      description: '',
+      file: {
+        contentType: "image/jpeg",
+        fileName: "Product3.jpg",
+        url: "//images.ctfassets.net/2gssinhyknyf/2CcI7wMc7sSfyTqTqesSqN/f64a6c0a4fe39a9db3affad3db9ffd9b/Product3.jpg",
+        details: {
+          image: { width: 3900, height: 4875 },
+          size: 1639283
+        }
+      },
+    },
+  }]
 
   return (
     <>
       <Header />
-      {products.map((product, index) => (
+      {product.map((product, index) => (
         <ProductCard key={index} product={product} />
       ))}
     </>
